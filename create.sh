49a9db168ad03c9f2aaa96d6a80a5f4dda3eabe3
axelca <cuevas.axel@uabc.edu.mx>
2023-10-24 20:48:29 -0700

    Third commit, add README.md and final script updated

--- create.sh
+++ create.sh
@@ -3,24 +3,34 @@
 rm -rf webapp
 mkdir webapp
 cd webapp
-touch css.css
-touch js.js
+mkdir css
+mkdir js
 mkdir docs
 cd docs
-touch text.txt
-touch python.py
+mkdir text
+mkdir python
 cd ..
-touch img.jpg
+mkdir img
 
 for file in index.html style.css app.js main.py
     do
         touch ./"${file}"
     done
 
-curl -o meme.jpg "https://pm1.aminoapps.com/7021/417429318f6b29b6bc737fb95ba1ddafc9ba2decr1-720-642v2_hq.jpg"
 
+curl -o meme.jpg "https://pm1.aminoapps.com/7021/417429318f6b29b6bc737fb95ba1ddafc9ba2decr1-720-642v2_hq.jpg"
+cd docs
+cd text
 read -p "Introduce la cantidad de archivos a crear:" numfiles
     for x in $( seq 1 $numfiles )
     do
         echo "Archivo ${x}" > "file${x}.txt"
     done
+cd ..
+cd ..
+
+    mv style.css css
+    mv main.py docs/python
+    mv app.js js
+    mv meme.jpg img
+    
